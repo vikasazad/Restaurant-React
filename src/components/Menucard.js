40b77ee69c998590c6b2ec4e2fa57efd66cdfc49
Vikas Kumar Azad <surajnimeshh1000@gmail,com>
2021-12-02 21:30:34 +0530

    Adding Reataurant

---
+++ src/components/Menucard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+// menuData recieved from restaurant.js jo vha se as props pass hoga same name yha bhi use krna h
+const MenuCard = ({ menuData }) => {
+  //   console.log(menuData);
+
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((curElem) => {
+          // Below code is example of destructuring by defining all fields of menuapi as curElem now we dont have to write (curElem.id) only (id) will for in key 
+          const { id, name, category, image, description } = curElem;
+
+          return (
+            <>
+              <div className="card-container" key={id}>
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">{id}</span>
+                    <span className="card-author subtle"> {category}</span>
+                    <h2 className="card-title"> {name} </h2>
+                    <span className="card-description subtle">
+                      {description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={image} alt="images" className="card-media" />
+
+                  <span className="card-tag  subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default MenuCard;
